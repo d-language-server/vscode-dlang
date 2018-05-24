f1686d2bd1dc28059ceaac617ebffcb3c76e0d94
Laurent Tréguier <laurent@treguier.org>
2018-05-24 21:08:14 +0200

    Support final dls.upgradDls notifications

--- src/extension.ts
+++ src/extension.ts
@@ -6,6 +6,7 @@ import * as cp from 'child_process';
 import * as rl from 'readline';
 import * as vsc from 'vscode';
 import * as lc from 'vscode-languageclient';
+import * as bytes from 'bytes';
 
 export function activate(context: vsc.ExtensionContext) {
     let dlsPath = vsc.workspace.getConfiguration('d').get<string>('dlsPath')
@@ -27,7 +28,7 @@ export function activate(context: vsc.ExtensionContext) {
     let dub = vsc.workspace.getConfiguration('d').get<string>('dubPath') || 'dub';
     let options: vsc.ProgressOptions = {
         location: vsc.ProgressLocation.Notification,
-        title: 'Installing DLS... (this might take a few minutes)',
+        title: 'Installing DLS...',
         cancellable: false
     };
 
@@ -35,10 +36,26 @@ export function activate(context: vsc.ExtensionContext) {
         new Promise(resolve => cp.spawn(dub, ['fetch', 'dls']).on('exit', resolve))
             .then(() => new Promise(resolve => {
                 let bootstrap = cp.spawn(dub, ['run', '--quiet', 'dls:bootstrap', '--', '--progress']);
+                let totalSize = 0;
+                let currentSize = 0;
                 bootstrap.stdout.on('data', data => dlsPath += data.toString())
                     .on('end', resolve);
                 rl.createInterface(bootstrap.stderr)
-                    .on('line', (line: number) => progress.report({ increment: line }));
+                    .on('line', (line: string) => {
+                        const size = Number(line);
+
+                        if (line === 'extract') {
+                            progress.report({ message: 'Extracting DLS...' });
+                        } else if (totalSize === 0) {
+                            totalSize = size;
+                        } else {
+                            currentSize = size;
+                            progress.report({
+                                increment: 100 * (size - currentSize) / totalSize,
+                                message: `Downloading... (${bytes(size)}/${bytes(totalSize)})`
+                            });
+                        }
+                    });
             }))
             .then(() => launchServer(context, dlsPath)));
 }
@@ -61,7 +78,7 @@ function launchServer(context: vsc.ExtensionContext, dlsPath: string) {
         synchronize: { configurationSection: 'd.dls' },
         initializationOptions: {
             lspExtensions: {
-                upgradeProgress: true
+                upgradeDls: true
             }
         }
     };
@@ -72,15 +89,21 @@ function launchServer(context: vsc.ExtensionContext, dlsPath: string) {
             title: 'Upgrading DLS...',
             cancellable: false
         };
-        let task: vsc.Progress<{ increment: number, message: string }>;
-        let total = 0;
+        let task: vsc.Progress<{ increment?: number, message?: string }>;
+        let totalSize = 0;
+        let currentSize = 0;
         let resolve: lc.GenericNotificationHandler;
         let updatePath = (path: string) => vsc.workspace.getConfiguration('d').update('dlsPath', path, vsc.ConfigurationTarget.Global);
         client.onNotification('$/dls.upgradeDls.start', () => vsc.window.withProgress(options, t => new Promise(r => { task = t; resolve = r })));
-        client.onNotification('$/dls.upgradeDls.progress', (progress: number) => {
-            task.report({ increment: progress, message: `Upgrading DLS [${total}%]` });
-            total += progress;
+        client.onNotification('$/dls.upgradeDls.totalSize', (size: number) => { totalSize = size; });
+        client.onNotification('$/dls.upgradeDls.currentSize', (size: number) => {
+            task.report({
+                increment: 100 * (size - currentSize) / totalSize,
+                message: `Downloading... (${bytes(size)} / ${bytes(totalSize)})`
+            });
+            currentSize = size;
         });
+        client.onNotification('$/dls.upgradeDls.extract', () => task.report({ message: 'Extracting...' }));
         client.onNotification('$/dls.upgradeDls.stop', () => resolve());
         client.onNotification('dls/didUpdatePath', updatePath);
     });
